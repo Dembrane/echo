8b069a07f828445d1d675dce872349dc1f252c15
Roy <arindamroy11235@gmail.com>
2025-04-29 09:45:20 +0000

    Directus tables added for the citations

--- echo/directus/directus-sync.config.js
+++ echo/directus/directus-sync.config.js
@@ -6,7 +6,8 @@ module.exports = {
   preserveIds: ['roles', 'policies', 'dashboards', 'panels'],
   exclude: {
     collections: [
-      /lightrag_.*/  // This regex pattern will match any collection that starts with "lightrag_"
+      "lightrag_chunk_graph_map","lightrag_doc_chunks","lightrag_doc_full","lightrag_doc_status",
+      "lightrag_llm_cache","lightrag_vdb_entity","lightrag_vdb_relation","lightrag_vdb_transcript"  // This regex pattern will match any collection that starts with "lightrag_"
     ]
   }
 };
